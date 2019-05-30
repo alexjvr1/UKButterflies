ca367a8b75d200017a69f5571ed8e39d4592d311
ritarasteiro <5339671+ritarasteiro@users.noreply.github.com>
2019-05-30 17:04:05 +0100

    Update grep_vcf_info.sh

--- grep_vcf_info.sh
+++ grep_vcf_info.sh
@@ -14,15 +14,16 @@ do
 done
 
 rm temp*
-#2.CALCULATE number of snps per contig and per species  (USING BCFTOOLS: more efficient than my previous bash script): 
+#2.CALCULATE number of snps per contig and per species  
+#(USING BCFTOOLS: more efficient than my previous bash script. It runs in a few minutes): 
 
 for i in {1..3}
 do
 tabix -p vcf G$i.modern_variants.bial.noindel.qs20.cov40.mdp10Mdp2200.vcf.gz #needs indexing
-#just contigs with snps
+#extract contigs with snps
 bcftools index -s G$i.modern_variants.bial.noindel.qs20.cov40.mdp10Mdp2200.vcf.gz > G$i.seqinfo_variants
 
-#compare files:create file for contigs without variants
+#compare files:create a file for contigs without variants
 awk 'FNR==NR{a[$1];next};!($1 in a)' G$i.seqinfo_variants G$i.id_length.txt > G$i.seqinfo_novariants
 done
 
