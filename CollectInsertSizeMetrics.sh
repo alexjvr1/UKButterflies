a323e17e8d42f04a35f7fb0d1822be3ec0243538
alexjvr1 <alexjvr1@users.noreply.github.com>
2019-10-23 08:24:45 +0100

    Update CollectInsertSizeMetrics.sh

--- CollectInsertSizeMetrics.sh
+++ CollectInsertSizeMetrics.sh
@@ -4,24 +4,26 @@
 #PBS -l mem=16gb #RAM
 #PBS -l walltime=10:00:00 ##wall time.
 #PBS -j oe  #concatenates error and output files (with prefix job1)
+#PBS -t 1-41
 
 
 #run job in working directory
 cd $PBS_O_WORKDIR
 pwd
-#cd WPA #uncomment when running locally
-#pwd
 
 #Load modules
 module load apps/picard-2.20.0
 module load languages/java-jdk-1.8.1-44
 
 #Define variables
+NAME=$(sed "${PBS_ARRAYID}q;d" museum.bam.names)
 
-for i in $(ls *bam); do java -jar /cm/shared/apps/Picard-2.20.0/picard.jar CollectInsertSizeMetrics \
-	I=$i \
-      O=$i.insert_size_metrics.txt \
-      H=$i.insert_size_histogram.pdf \
+
+#Run script
+java -jar /cm/shared/apps/Picard-2.20.0/picard.jar CollectInsertSizeMetrics \
+	I=${NAME} \
+      O=${NAME}.insert_size_metrics.txt \
+      H=${NAME}.insert_size_histogram.pdf \
       M=0.5
 
 
